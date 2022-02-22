cd311a5814aff98c8444a9971109cbb424e9b5d4
NikkeJJ <niklas.juvenius@metropolia.fi>
2022-02-22 13:28:38 +0200

    test

---
+++ app/src/main/java/course/labs/hyte_projekti/AddingActivity.java
@@ -0,0 +1,14 @@
+package course.labs.hyte_projekti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding);
+    }
+}
